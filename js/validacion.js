eec8d514c92799011f2605b1949facb5d112fd5c
CorbalanLucas <129867324+CorbalanLucas@users.noreply.github.com>
2023-04-24 00:11:32 -0300

    agregar php y sql

--- js/validacion.js
+++ js/validacion.js
@@ -1,54 +1,60 @@
 function validar(){
+    let valido = true;
 
     let nombre=document.getElementById("inputNombre").value;
-    let descripcion=document.getElementById("inputDescripcion").value.length;
+    let descripcion=document.getElementById("inputDescripcion").value;
     let imagen=document.getElementById("inputImagen").value;
     let plataforma=document.getElementById("inputPlataforma").value;
-    let url=document.getElementById("inputUrl").value.length;
+    let url=document.getElementById("inputUrl").value;
     let genero=document.getElementById("inputGenero").value;
         
         if (nombre=='') {
             document.getElementById('obligatorioNombre').style.display='inline';
+            valido=false;
         }else{
             document.getElementById('obligatorioNombre').style.display='none';
         }
         
         //valido max 250 caracteres, puede estar vacio
-        if (descripcion>250){            
-            document.getElementById('obligatorioDescripcion').style.display='inline';              
+        if (descripcion.length>250){            
+            document.getElementById('obligatorioDescripcion').style.display='inline';
+            valido=false;              
         }else{            
             document.getElementById('obligatorioDescripcion').style.display='none';
         }
 
         if (imagen==''){
-            document.getElementById('obligatorioImg').style.display='inline';            
+            document.getElementById('obligatorioImg').style.display='inline';
+            valido=false;
         }else{
             document.getElementById('obligatorioImg').style.display='none';
         }
         
         if (plataforma==''){
-            document.getElementById('obligatorioPlataforma').style.display='inline';              
+            document.getElementById('obligatorioPlataforma').style.display='inline';
+            valido=false;
         }else{
             document.getElementById('obligatorioPlataforma').style.display='none';
         }
         
         if (genero==''){
             document.getElementById('obligatorioGenero').style.display='inline';
+            valido=false;
         }else{
             document.getElementById('obligatorioGenero').style.display='none';
         }
         
         //valido max 80 caracteres, puede estar vacio
-        if (url>80){
-            document.getElementById('obligatorioURL').style.display='inline';         
+        if (url.length>80){
+            document.getElementById('obligatorioURL').style.display='inline';
+            valido=false;
         }else{
             document.getElementById('obligatorioURL').style.display='none';  
         }
         
-        event.preventDefault();    
-        
-
+        event.preventDefault();
 }
+
 
 function resetCampos(){
 
