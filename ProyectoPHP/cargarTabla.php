2a06f15ad334edc04c6e6d134ccb8a0d7421a7ae
CorbalanLucas <129867324+CorbalanLucas@users.noreply.github.com>
2023-04-24 00:11:06 -0300

    Delete ProyectoPHP directory

--- ProyectoPHP/cargarTabla.php
+++
@@ -1,4 +0,0 @@
-<?php
-include('conexionBD.php');
-mysqli_query($link, "INSERT INTO `juegos` (`id`, `nombre`, `imagen`, `tipo_imagen`, `descripcion`, `url`, `id_genero`, `id_plataforma`) VALUES (NULL, nombre, '', '', descripcion, url, genero, plataforma);")
-?>
